869007b75990d95bcf91905e7274ba7443a4b492
Adil <Aadiltahir116@gmail.com>
2022-03-23 18:46:44 +0500

    updated code

--- app/src/main/java/com/example/foode/register_now.java
+++ app/src/main/java/com/example/foode/register_now.java
@@ -5,11 +5,8 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
-import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.ListView;
-import android.widget.Spinner;
 import android.widget.Toast;
 
 public class register_now extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
