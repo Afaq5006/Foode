90b35faf120d9d24c91230642a4fbaca560bb5b6
master <afaq5006@gmail.com>
2022-04-14 03:51:49 -0700

    Add Menu Page

--- app/src/main/java/com/example/foode/Dashboard.java
+++ app/src/main/java/com/example/foode/Dashboard.java
@@ -1,14 +1,28 @@
 package com.example.foode;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class Dashboard extends AppCompatActivity {
+
+    CardView menu;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dashboard);
+
+        menu = findViewById(R.id.Card_menu);
+        menu.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Dashboard.this, Menu.class);
+                startActivity(intent);
+            }
+        });
     }
 }
