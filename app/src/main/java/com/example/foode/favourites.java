7c74ba4d46e840c6a748dfed9f70fa19580ad8d0
master <afaq5006@gmail.com>
2022-05-09 21:28:47 -0700

    Add to Rating Bar.

--- app/src/main/java/com/example/foode/favourites.java
+++ app/src/main/java/com/example/foode/favourites.java
@@ -3,12 +3,29 @@ package com.example.foode;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.RatingBar;
+import android.widget.Toast;
 
 public class favourites extends AppCompatActivity {
+    RatingBar ratingBar;
+    Button btnSubmit;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_favourites);
+
+        ratingBar = findViewById(R.id.rating_bar);
+        btnSubmit = findViewById(R.id.btn_submit);
+
+        btnSubmit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String s = String.valueOf(ratingBar.getRating());
+                Toast.makeText(getApplicationContext(), s+"Star", Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
