a9623245b440ba612622b670164263d4c4941ec1
master <afaq5006@gmail.com>
2022-05-14 23:17:41 -0700

    contact Number.

--- app/src/main/java/com/example/foode/contact_us.java
+++ app/src/main/java/com/example/foode/contact_us.java
@@ -3,12 +3,13 @@ package com.example.foode;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.TextView;
 
 public class contact_us extends AppCompatActivity {
-    TextView txtmail;
+    TextView txtmail, txtnum;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,6 +21,17 @@ public class contact_us extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 Intent intent = new Intent(contact_us.this, contact_mail.class);
+                startActivity(intent);
+            }
+        });
+
+        txtnum = findViewById(R.id.txt_num);
+        txtnum.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String wpurl="http://wa.me/+923086045982?text=Hi,Is Any Available ?";
+                Intent intent=new Intent(Intent.ACTION_VIEW);
+                intent.setData(Uri.parse(wpurl));
                 startActivity(intent);
             }
         });
